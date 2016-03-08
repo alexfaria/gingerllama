a10a2c59cfa5861e6dddb881801fc01f6cb844b1
Alex Faria <simaofaria11@gmail.com>
2016-03-08 16:13:29 +0000

    fixed bugs | changed to flask-sqlalchemy

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class ProdConfig(object):
     PORT = os.getenv('PORT', 5000)
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SECRET_KEY = '\xf4\xf3\xda\x83\xab\x0f\xf8\x92DZ\xa2\x17\xe0\xdd\xd8\xa4\xdc\xdd\xa9[l\xf9~\x1f'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 class DevConfig(ProdConfig):
     DEBUG = True
