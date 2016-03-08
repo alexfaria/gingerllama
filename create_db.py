fbb29da12e02050bcc8ae0b07d12b973a98629fa
Alex Faria <simaofaria11@gmail.com>
2016-03-08 15:34:35 +0000

    added create_db.py

---
+++ create_db.py
@@ -0,0 +1,13 @@
+from app import app
+from flask.ext.sqlalchemy import SQLAlchemy
+from models import List
+
+db = SQLAlchemy(app)
+db.create_all()
+
+db.session.add(List(title = 'Check List 1'))
+db.session.add(List(title = 'Check List 2'))
+db.session.add(List(title = 'Check List 3'))
+db.session.add(List(title = 'Check List 4'))
+
+db.session.commit()
