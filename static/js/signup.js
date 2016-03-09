9a742c99d84f431160c147c39ff4894b02b5e9e3
Alex Faria <simaofaria11@gmail.com>
2016-03-09 19:52:59 +0000

    added signup page

---
+++ static/js/signup.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+    // $('#inputConfirmPassword').bind('input propertychange', function(){
+    //     var pass1 = $(this).parent().find('#inputPassword').val();
+    //     var pass2 = $(this).val();
+    //     if (pass1 != pass2){
+    //         $(this).toggleClass('has-error');
+    //         console.log('error');
+    //     }
+    // });
+
+    // $('form').on('submit', function(){
+    //     var pass1 = $(this).find('#inputPassword').val();
+    //     var pass2 = $(this).find('#inputConfirmPassword').val();
+    //     if(pass1 != pass2){
+    //         $(this).find('#inputConfirmPassword').parent().toggleClass('has-error');
+    //         return false;
+    //     }
+    //     return true;
+    // });
+
+});
