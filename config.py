e1ad099970606bcb3c53021ca80f208cbce3bd3d
Alex Faria <simaofaria11@gmail.com>
2016-03-09 20:03:03 +0000

    changed SECRET_KEY to be read from the env variables

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class ProdConfig(object):
     DEBUG = False
     PORT = os.getenv('PORT', 5000)
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    SECRET_KEY = '\xf4\xf3\xda\x83\xab\x0f\xf8\x92DZ\xa2\x17\xe0\xdd\xd8\xa4\xdc\xdd\xa9[l\xf9~\x1f'
+    SECRET_KEY = os.environ['SECRET_KEY']
     SQLALCHEMY_TRACK_MODIFICATIONS = True       # surpress annoying message
 
 class DevConfig(ProdConfig):
