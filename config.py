b947a6fd21f43825ed73725bebd4f5436374c4f1
Alex Faria <simaofaria@hotmail.com>
2016-03-09 20:09:00 +0000

    use random secret key if not set as env variable

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class ProdConfig(object):
     DEBUG = False
     PORT = os.getenv('PORT', 5000)
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    SECRET_KEY = os.environ['SECRET_KEY']
+    SECRET_KEY = os.getenv('SECRET_KEY', os.urandom(24).encode('hex'))
     SQLALCHEMY_TRACK_MODIFICATIONS = True       # surpress annoying message
 
 class DevConfig(ProdConfig):
