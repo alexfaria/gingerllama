093bca9d853f547d2fcc54435692fd0a930c753f
alexfaria <simaofaria@hotmail.com>
2016-07-17 00:49:53 +0000

    default config

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class ProdConfig(object):
     DEBUG = False
     PORT = os.getenv('PORT', 5000)
-    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///database.db')
     SECRET_KEY = os.getenv('SECRET_KEY', os.urandom(24).encode('hex'))
     SQLALCHEMY_TRACK_MODIFICATIONS = True       # surpress annoying message
 
